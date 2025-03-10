2eaa01549ce7f5ca532d9463d6be5b7f7a666f5d
beniucahoihoang <khang.huynh.210218@student.fulbright.edu.vn>
2025-03-10 16:16:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const links = [
-    "https://example.com",
+    "https://www.nhathuocankhang.com/ban-tin-suc-khoe/15-thoi-quen-tot-cho-suc-khoe-moi-ngay-khong-the-bo-qua-1469128",
     "https://another-random-link.com",
-    "https://your-surprise-ending.com",
-    "https://one-more-chaotic-link.com"
+    "https://tuoitre.vn/co-gi-o-mang-den-thien-duong-hong-ma-ca-ngan-du-khach-ve-tham-20231231183607197.htm",
+    "https://one-more-chaotic-link.com",
+    "https://thegioilamvuon.com/chau-nhua-trong-cay/?srsltid=AfmBOopeDxgKPe1sqAvpPcJPGXg8hMlL_g9H8rOcFFul_AMRDFX7ykW9",
+    "https://tinder.com/",
+    "https://bet888vn.net/vn?utm_source=seo&utm_medium=auto&utm_campaign=giz-ads-1",
+    "https://nhipcaudautu.vn/phong-cach-song/di-tim-gia-tri-cho-cong-dong-lgbt-3357185/",
+    "https://www.penguin.co.uk/series/EVLC/everymans-library-classics",
+    "https://genius.com/Lola-young-messy-lyrics",
+    "https://www.crescentmall.com.vn/tenants/ovs",
+    "https://archive.org/details/the-pillow-book/page/n4/mode/1up",
+
 ];
 
 document.getElementById("randomLink").addEventListener("click", function() {
